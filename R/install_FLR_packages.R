1452e1e90167326c326eb38253e3e8e60412b699
Alessandro Orio <aleorio@yahoo.it>
2014-07-16 10:39:44 +0200

    Added a line

--- R/install_FLR_packages.R
+++ R/install_FLR_packages.R
@@ -15,6 +15,7 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
+
 #--------------------------------------------------------------------------
 # This small script installs the latest FLR packages.
 # They are for R >= 3.0
